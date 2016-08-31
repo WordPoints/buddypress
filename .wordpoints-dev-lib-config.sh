74fdd72416bd4bd51e59fdd5b9da42fc784e3a4a
JDGrimes <jdg@codesymphony.co>
2016-08-31 10:45:44 -0400

    Correct var name
    See #10

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -10,7 +10,7 @@ export BP_TESTS_DIR=/tmp/buddypress/tests/phpunit
 # Install BuddyPress.
 install-buddypress() {
 
-	mkdir -p "$WP_DEVELOP_DIR"
+	mkdir -p "$BP_DEVELOP_DIR"
 	curl -L "https://github.com/buddypress/BuddyPress/archive/$BP_VERSION.tar.gz" \
 		| tar xvz --strip-components=1 -C "$BP_DEVELOP_DIR"
 	ln -s  "$BP_DEVELOP_DIR"/src "$WP_CORE_DIR"/wp-content/plugins/buddypress
