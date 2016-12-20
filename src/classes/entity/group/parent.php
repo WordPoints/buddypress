48f11d6fed3937f00f4f55efef6375cbe36baecc
JDGrimes <jdg@codesymphony.co>
2016-12-20 10:53:23 -0500

    Be more specific
    See #3

--- src/classes/entity/group/parent.php
+++ src/classes/entity/group/parent.php
@@ -39,7 +39,7 @@ class WordPoints_BP_Entity_Group_Parent
 	 * @since 1.0.0
 	 */
 	public function get_title() {
-		return __( 'Parent', 'wordpoints-bp' );
+		return __( 'Parent Group', 'wordpoints-bp' );
 	}
 }
 
