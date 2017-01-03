a61475ea782bb5569db787e73f902d262416fcfa
JDGrimes <jdg@codesymphony.co>
2017-01-03 11:03:30 -0500

    Try installing BP first
    See #10

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -10,22 +10,16 @@ export BP_TESTS_DIR=/tmp/buddypress/tests/phpunit
 # Install BuddyPress.
 install-buddypress() {
 
-	# See https://github.com/WordPoints/buddypress/issues/10.
-	if [[ $TRAVIS_PHP_VERSION = hhvm || $TRAVIS_PHP_VERSION = nightly ]]; then
-		sleep 5
-	fi
-
 	mkdir -p "$BP_DEVELOP_DIR"
 	curl -L "https://github.com/buddypress/BuddyPress/archive/$BP_VERSION.tar.gz" \
 		| tar xvz --strip-components=1 -C "$BP_DEVELOP_DIR"
-	ln -s  "$BP_DEVELOP_DIR"/src "$WP_CORE_DIR"/wp-content/plugins/buddypress
 }
 
 # Override commands.
 wordpoints-dev-lib-config() {
 
 	# Install BuddyPress for the PHPUnit pass.
-	alias setup-phpunit="setup-phpunit; install-buddypress";
+	alias setup-phpunit='install-buddypress; setup-phpunit; ln -s "$BP_DEVELOP_DIR"/src "$WP_CORE_DIR"/wp-content/plugins/buddypress';
 }
 
 # EOF
